61edd1c2bb49346e8926ce21847c926982f1a17a
Joy Best <fasterthanmomentum@gmail.com>
2019-09-14 15:54:03 -0400

    just added the func update function

---
+++ EmojiDictionary/EmojiTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiDictionary
+//
+//  Created by JOY BEST on 9/14/19.
+//  Copyright © 2019 JOY BEST. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    @IBOutlet weak var symbolLabel: UILabel!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+  
+    func update(with emoji: Emoji) {
+        symbolLabel.text = emoji.symbol
+        nameLabel.text = emoji.name
+        descriptionLabel.text = emoji.description
+    }
+    
+}
